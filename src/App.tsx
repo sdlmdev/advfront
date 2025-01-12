d5bb9c7955613183acf1275886d7bbcdeb71abe2
sdlmdev <sdlmdev@yandex.ru>
2025-01-13 00:48:42 +0300

    feat: добавил стилизацию тем

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import {MainPageAsync} from "./pages/MainPage/MainPageAsync.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync.async";
 import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Suspense fallback={<div>load...</div>}>

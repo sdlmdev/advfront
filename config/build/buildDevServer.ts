521c234e2de93551ac089d49d0f3bd02982c162d
sdlmdev <sdlmdev@yandex.ru>
2025-01-12 18:55:46 +0300

    feat: настроил вебпак

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,11 @@
+import { BuildOptions } from './types/config';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+export const buildDevServer = (options: BuildOptions): DevServerConfiguration => {
+  const { port } = options;
+
+  return {
+    port,
+    open: true,
+  }
+};
